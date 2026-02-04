697f7b6b87defa8740eb3dd51da1285f647494a2
Sirajudeen.R <sira.deen@gmail.com>
2026-02-04 22:26:43 +0530

    sgs

--- UI/ReactStore/Index.ts
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-// import { requestReducer } from "./requestReducer";
-//import { authReducer } from "./auth-reducer";
-  
-export const rootReducer = combineReducers({ } as any);
